17577194e4fb58a896fbaa0248a7aee889d5a54f
Lucas <lucasvonelm@gmail.com>
2023-11-03 11:09:52 -0300

    Started Frontend

---
+++ events-frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Eventos</h1>
+        <p>Frontend para visualizar eventos</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
